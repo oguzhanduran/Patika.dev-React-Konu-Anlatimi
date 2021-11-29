3ec03a3b6c5662e1a3e7c5910a94877b5d2cb08d
OGUZHAN DURAN <oguzhnduran@gmail.com>
2021-11-29 11:20:57 +0300

    Bölüm Sonu Kazanımlarına Geçildi.

---
+++ 3-States.js
@@ -0,0 +1,6 @@
+// State componentlerin üzerinde değerinin değişme potansiyeli olan bütün verileri tutan bir javascript objesidir. Component'in herhangi bir anında değeri değişme potansiyeli olan bir veri state olarak tanımlanır ve bu state değiştiği andada ilgili component render edilerek değişim ekrana yazdırılmış olur.
+
+// terminale npx create-react-app dedikten sonra projenin ismini yazıp enter'a basıyorduk normalde eğer buraya proje ismi yerine . koyarsak direk olarak bulunduğumuz dizine ilgili projenin kurulumunu yapacaktır. Yani ek olarak bir klasör daha açmayaacak states adında bir tane dizi açtık ve onun altına bu kurulumu gerçekleştirdik. İşlem tamamlandıktan sonra bir states'i nasıl tanımlayabileceğimizi ve component'in içinde render ettiğimiz jsx'in içinde bu state elemanlarını nasıl kullanabileceğimizi öğrenmeye çalışcaz. Yüklendikten sonra yarn start diyerek projemizi çalıştırıyoruz. 
+
+
+// Bölüm Sonu Kazanımları (States)
