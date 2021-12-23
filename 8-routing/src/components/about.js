0b34c2ecb754a45844daef85e8021d01c6c32c1d
OGUZHAN DURAN <oguzhnduran@gmail.com>
2021-12-23 11:43:05 +0300

    Editted

--- 8-routing/src/components/about.js
+++ 8-routing/src/components/about.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function about() {
+function About() {
     return (
         <div>
             <h2>About</h2>
@@ -8,4 +8,4 @@ function about() {
     )
 }
 
-export default about
+export default About
--- 8-routing/src/components/home.js
+++ 8-routing/src/components/home.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function home() {
+function Home() {
     return (
         <div>
             <h2>Home</h2>
@@ -8,4 +8,4 @@ function home() {
     )
 }
 
-export default home
+export default Home
